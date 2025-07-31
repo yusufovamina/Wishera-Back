50d7dd7db948510b82bc886f5f653e44c1753513
yusufovamina <yusufovamina@gmail.com>
2025-07-31 13:15:22 +0400

    microservice structure applied

---
+++ auth-service/DTO/AuthDTO.cs
@@ -0,0 +1,55 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace WishlistApp.DTO
+{
+    public class RegisterDTO
+    {
+        [Required]
+        [StringLength(50, MinimumLength = 3)]
+        public required string Username { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public required string Email { get; set; }
+
+        [Required]
+        [StringLength(100, MinimumLength = 6)]
+        public required string Password { get; set; }
+    }
+
+    public class LoginDTO
+    {
+        [Required]
+        [EmailAddress]
+        public required string Email { get; set; }
+
+        [Required]
+        public required string Password { get; set; }
+    }
+
+    public class ForgotPasswordDTO
+    {
+        [Required]
+        [EmailAddress]
+        public required string Email { get; set; }
+    }
+
+    public class ResetPasswordDTO
+    {
+        [Required]
+        public required string Token { get; set; }
+
+        [Required]
+        [StringLength(100, MinimumLength = 6)]
+        public required string NewPassword { get; set; }
+    }
+
+    public class AuthResponseDTO
+    {
+        public required string Token { get; set; }
+        public required string UserId { get; set; }
+        public required string Username { get; set; }
+        public required string Email { get; set; }
+        public string? AvatarUrl { get; set; }
+    }
+} 
